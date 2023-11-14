549392964dcdee95c0fde311539e38e81204af3f
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-11-14 13:42:10 -0600

    ftr: Agregando login con comunicacion a back

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-
-import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      <Login />
     </main>
   );
 }
